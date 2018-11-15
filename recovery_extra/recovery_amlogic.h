a4f9774a7418d5379ab26eec9267dfa1fbb69ef1
Zhigang <zhigang.yu@amlogic.com>
2018-11-15 16:21:51 +0800

    recovery: add recovery amlogic License for Android P [1/1]
    PD#SWPL-2111
    
    Problem:
    There is no license of amlogic recovery Sourcefiles
    
    Solution:
    add recovery amlogic License for Android P
    
    Verify:
    Verify by Android P ampere
    
    Change-Id: Ib68480e4f3227f3226f63969af4602ad67b35b75
    Signed-off-by: Zhigang <zhigang.yu@amlogic.com>

--- recovery_extra/recovery_amlogic.h
+++ recovery_extra/recovery_amlogic.h
@@ -1,3 +1,12 @@
+/*
+* Copyright (c) 2014 Amlogic, Inc. All rights reserved.
+* *
+This source code is subject to the terms and conditions defined in the
+* file 'LICENSE' which is part of this source code package.
+* *
+Description:
+*/
+
 #ifndef _RECOVERY_INSTALL_AMLOGIC_H_
 #define _RECOVERY_INSTALL_AMLOGIC_H_
 
